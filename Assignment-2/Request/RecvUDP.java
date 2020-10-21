93ec62af8795b7d3295ea884565f5b6e8a56d85a
azg0066 <42651825+azg0066@users.noreply.github.com>
2020-10-20 20:48:18 -0500

    Update RecvUDP.java

--- Assignment-2/Request/RecvUDP.java
+++ Assignment-2/Request/RecvUDP.java
@@ -25,6 +25,43 @@ public class RecvUDP {
 
       System.out.println("Received Binary-Encoded Request");
       System.out.println(receivedRequest);
+      
+      //checksum checker
+      byte l = receivedRequest.length;
+      short RID = receivedRequest.RID;
+      int x = receivedRequest.x;
+      int a3 = receivedRequest.a3;
+      int a2 = receivedRequest.a2;
+      int a1 = receivedRequest.a1;
+      int a0 = receivedRequest.a0;
+      byte[] prechecksum = {l, (byte)RID, (byte)a3, (byte)a2, (byte)a1, (byte)a0};
+
+      byte checksum = checksum(prechecksum);
+
+
+
+      float answer = calculator(receivedRequest);
+
+      int error = 0;
+      if (packet.getLength() != 9) {
+          error = 127;
+      }
+      if (checksum != receivedRequest.checksum) {
+          error = 63;
+      }
+      if (packet.getLength() != 9) {
+          error = 127;
+      }
+
+      byte[] prechecksum2 = {9, (byte)RID, (byte)error, (byte)answer};
+      byte newChecksum = checksum(prechecksum2);
+      ResponseEncoder encoder = (args.length == 2 ? new ResponseEncoderBin(args[2]) : new ResponseEncoderBin());
+      Response response = new Response(RID, error, answer, newChecksum);
+      byte[] codedResponse = encoder.encode(response);
+
+      packet.setData(codedResponse);
+      System.out.println(codedResponse);
+      sock.send(packet);      
     }
 
   }
