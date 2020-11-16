f0e8580937ef2ed84119da12a674ffa029d4261a
connormorris <connor3090@bellsouth.net>
2020-11-16 15:47:27 -0600

    Assignment 3 commit

---
+++ Assignment-3/Request/Request.java
@@ -0,0 +1,42 @@
+public class Request {
+    
+    public byte length;
+    public short RID;
+    public int x;
+    public int a3;
+    public int a2;
+    public int a1;
+    public int a0;
+    public byte checksum;
+
+    public Request(short RID, int x, int a3, int a2, int a1, int a0, byte checksum) {
+        this.length=9;
+        this.RID = RID;
+        this.x = x;
+        this.a3 = a3;
+        this.a2 = a2;
+        this.a1 = a1;
+        this.a0 = a0;
+        this.checksum = checksum;
+    }
+
+    public String toString() {
+        final String EOLN = java.lang.System.getProperty("line.separator");
+        String value = "";
+        if (a3 > 0) {
+            value += a3 + "x^3";
+        }
+        if (a2 > 0) {
+            value += " + " + a2 + "x^2";
+        }
+        if (a1 > 0) {
+            value += " + " + a1 + "x";
+        }
+        if (a0 > 0) {
+            value += " + " + a0 + EOLN;
+        }
+        value += "Solving for x = " + x + EOLN;
+        value += "Request ID = " + RID;
+        return value;
+    }
+}
